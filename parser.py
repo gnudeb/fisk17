61fd9ab9d48d58aca73502906d9acc1ad48f8916
gnudeb <gnudeb0@gmail.com>
2018-09-02 15:45:37 +0300

    Add docstrings to `Terminal` and subclasses

--- parser.py
+++ parser.py
@@ -19,6 +19,11 @@ class UnmatchedProduction(Exception):
 
 
 class Production:
+    """
+    An abstract production.
+
+    All concrete productions inherit from this class.
+    """
     def __init__(self, name):
         self.name = name
 
@@ -32,6 +37,12 @@ class Production:
 
 
 class NonTerminal(Production):
+    """
+    A non-terminal production.
+
+    It will match given productions in strict order. If any of given
+    productions raise exceptions, it is up to caller to catch them.
+    """
     def __init__(self, name, productions):
         super().__init__(name)
         self.productions = productions
@@ -46,6 +57,14 @@ class NonTerminal(Production):
 
 
 class Terminal(Production):
+    """
+    A terminal production.
+
+    It will match any token that has the same name as `self.name`. If a token
+    cannot be matched, `Terminal` will raise `UnmatchedProduction`.
+
+    This class is equivalent to a terminal production in EBNF.
+    """
     def match(self, tokens: List[Token]) -> Tuple[Tree, Tokens]:
         first_token: Token = tokens[0]
         if first_token.name == self.name:
@@ -55,9 +74,10 @@ class Terminal(Production):
 
 class RepeatingProduction(Production):
     """
-    Match a given production zero or more times and return anonymous node.
+    An equivalent to `{production}` in EBNF.
 
-    This class is equivalent to `{production}` in EBNF.
+    It will match a given production zero or more times and return anonymous
+    node.
     """
     def __init__(self, production):
         super().__init__("")
