8fe1305d0684f364071f51bae16102b8b2b69e49
gnudeb <gnudeb0@gmail.com>
2019-02-04 01:28:36 +0200

    Refactor `Rule`

--- lexer.py
+++ lexer.py
@@ -17,6 +17,23 @@ class Rule:
         self.post_action = post_action
         self.ignore = ignore
 
+    def match(self, code, offset=0):
+        """Return result and size of matching `code` against `self.regex`"""
+        match = self.regex.match(code, offset)
+
+        if not match:
+            return None, 0
+
+        try:
+            result = match.groups()[0]
+        except IndexError:
+            result = match.group()
+
+        start, end = match.span()
+        size = end - start
+
+        return result, size
+
     def __repr__(self):
         return f"Rule({self.name})"
 
@@ -34,20 +51,16 @@ class Lexer:
         shift = 0
 
         while shift < len(code):
-            match = None
+            value = None
             for rule in self.rules:
-                match = rule.regex.match(code, pos=shift)
-                if match:
-                    _, shift = match.span()
-                    try:
-                        value = match.groups()[0]
-                    except IndexError:
-                        value = match.group()
+                value, size = rule.match(code, offset=shift)
+                if value:
+                    shift += size
                     if not rule.ignore:
                         yield rule.name, rule.mutator(value)
                     rule.post_action()
                     break
-            if not match:
+            if not value:
                 error_line = code.split("\n")[self.current_line-1]
                 raise LexerError(
                     f"Unexpected input on line {self.current_line}:\n"
